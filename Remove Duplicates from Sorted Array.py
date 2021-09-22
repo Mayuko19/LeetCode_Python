9f9ff7663696bfb62d55084bc6fc930c9920b42a
Mayuko.F <66619187+Mayuko19@users.noreply.github.com>
2021-09-22 12:33:45 +0900

    Update and rename 2_Remove Duplicates from Sorted Array.py to Remove Duplicates from Sorted Array.py

--- Remove Duplicates from Sorted Array.py
+++ Remove Duplicates from Sorted Array.py
@@ -9,13 +9,3 @@ class Solution(object):
                 pre += 1
                 nums[pre] = nums[curr]
         return pre + 1
-
-if __name__ == '__main__':
-    instance = Solution()
-    print(instance.removeDuplicates([1,1,2,2,3,3,3,4,4,5,5,]))
-
-    
-    
-    # input=[1,1,2,2,3,3,3,4,4,5,5,]
-    # output 5
-    
