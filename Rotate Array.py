97dd51182084468e1e3f785e19a4eb0bbb95f36d
Mayuko.F <66619187+Mayuko19@users.noreply.github.com>
2021-09-23 16:54:28 +0900

    Update Rotate Array.py

--- Rotate Array.py
+++ Rotate Array.py
@@ -10,3 +10,16 @@ class Solution:
             nums_2.append(nums[len(nums)-k+i])
             nums.pop(len(nums)-k+i)
         nums[:] = nums_2 + nums
+
+        
+        
+        
+from typing import List
+
+class Solution:
+    def rotate(self, nums: List[int], k: int) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        k %= len(nums)
+        nums[:] = nums[-k:] + nums[:-k]
