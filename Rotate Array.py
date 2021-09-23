cb46aba00f38a7a00b3c7cab04daac519adde005
Mayuko.F <66619187+Mayuko19@users.noreply.github.com>
2021-09-23 16:43:07 +0900

    Create Rotate Array.py

---
+++ Rotate Array.py
@@ -0,0 +1,12 @@
+from typing import List
+
+class Solution:
+    def rotate(self, nums: List[int], k: int) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        nums_2 =[]
+        for i in range(0, k):
+            nums_2.append(nums[len(nums)-k+i])
+            nums.pop(len(nums)-k+i)
+        nums[:] = nums_2 + nums
