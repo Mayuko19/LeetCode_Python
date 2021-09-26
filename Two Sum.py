854c7b380b48b09aea5d526be5a6b532fb04653f
Mayuko.F <66619187+Mayuko19@users.noreply.github.com>
2021-09-26 22:25:03 +0900

    Update Two Sum.py

--- Two Sum.py
+++ Two Sum.py
@@ -1,14 +1,11 @@
-class Solution(object):
-    def twoSum(self, nums, target):
-        nums_index = [(v, index) for index, v in enumerate(nums)]
-        nums_index.sort()
-        begin, end = 0, len(nums) - 1
-        while begin < end:
-            curr = nums_index[begin][0] + nums_index[end][0]
-            if curr == target:
-                return [nums_index[begin][1], nums_index[end][1]]
-            elif curr < target:
-                begin += 1
-            else:
-                end -= 1
-                
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        if nums is None:
+            return None
+        
+        for i in range(len(nums)):
+            mid = target - nums[i]
+            for j in range(i+1, len(nums)):
+                if nums[j] == mid:
+                    return i, j
+
