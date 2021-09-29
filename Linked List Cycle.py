c31f442777bc486718787d7ef03d8527e4bfe42a
Mayuko.F <66619187+Mayuko19@users.noreply.github.com>
2021-09-29 11:21:31 +0900

    Update Linked List Cycle.py

--- Linked List Cycle.py
+++ Linked List Cycle.py
@@ -17,3 +17,7 @@ class Solution(object):
             hashmap[head] = head
             head = head.next
         return False
+""""
+Runtime: 56 ms, faster than 66.13% of Python3 online submissions for Linked List Cycle.
+Memory Usage: 17.9 MB, less than 16.84% of Python3 online submissions for Linked List Cycle.
+""""
