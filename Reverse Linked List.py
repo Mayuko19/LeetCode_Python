54234f81c95e00bf49f6e96077c746d98c191c5e
Mayuko.F <66619187+Mayuko19@users.noreply.github.com>
2021-10-04 15:01:33 +0900

    Update and rename Reversed Linked List.py to Reverse Linked List.py

--- Reverse Linked List.py
+++ Reverse Linked List.py
@@ -18,3 +18,22 @@ class Solution:
 Runtime: 36 ms, faster than 71.74% of Python3 online submissions for Reverse Linked List.
 Memory Usage: 15.6 MB, less than 76.18% of Python3 online submissions for Reverse Linked List.
 """"
+
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        stack = []
+        while head:
+            stack.append(head.val)
+            head = head.next
+            
+        rev = ans = ListNode(0, None)
+        
+        while stack:
+            rev.next = ListNode(stack.pop())
+            rev = rev.next
+        return ans.next
+
+""""
+Runtime: 59 ms, faster than 11.85% of Python3 online submissions for Reverse Linked List.
+Memory Usage: 16.5 MB, less than 20.61% of Python3 online submissions for Reverse Linked List.
+""""
