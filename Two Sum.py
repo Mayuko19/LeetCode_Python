c50fa2fb9409b9a3e63c1018696453fef4987da3
Mayuko.F <66619187+Mayuko19@users.noreply.github.com>
2021-10-04 15:49:06 +0900

    Update Two Sum.py

--- Two Sum.py
+++ Two Sum.py
@@ -9,3 +9,17 @@ class Solution:
                 if nums[j] == mid:
                     return i, j
 
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        hashmap = {}
+        for i in range(len(nums)):
+            if nums[i] not in hashmap:
+                hashmap[target-nums[i]] = i 
+            else:
+                return hashmap[nums[i]], i                
+                
+""""
+Runtime: 112 ms, faster than 42.51% of Python3 online submissions for Two Sum.
+Memory Usage: 15.6 MB, less than 8.99% of Python3 online submissions for Two Sum
+""""
+
