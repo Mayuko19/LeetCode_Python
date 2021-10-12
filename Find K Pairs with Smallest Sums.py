366037d4cf037cded621847693e7ff3fc81a2947
Mayuko.F <66619187+Mayuko19@users.noreply.github.com>
2021-10-12 16:53:27 +0900

    Update Find K Pairs with Smallest Sums.py

--- Find K Pairs with Smallest Sums.py
+++ Find K Pairs with Smallest Sums.py
@@ -1,13 +1,18 @@
 class Solution:
     def kSmallestPairs(self, nums1, nums2, k):
-        if not nums1 or not nums2: return []
-        n, res, cnt, heap = len(nums2), [], 0, [(nums1[i] + nums2[0], i, 0) for i in range(len(nums1))]
+        if not nums1 or not nums2: 
+            return []
+        n  = len(nums2)
+        res = []
+        cnt = 0
+        heap = [(nums1[i] + nums2[0], i, 0) for i in range(len(nums1))]
         while heap and cnt < k:
             cnt += 1
             sm, i, j = heapq.heappop(heap)
             res.append([nums1[i], nums2[j]])
-            if j + 1 < n: heapq.heappush(heap, (nums1[i] + nums2[j + 1], i, j + 1))
-        return res       
+            if j + 1 < n: 
+                heapq.heappush(heap, (nums1[i] + nums2[j + 1], i, j + 1))
+        return res           
 """"
 Runtime: 669 ms, faster than 49.27% of Python3 online submissions for Find K Pairs with Smallest Sums.
 Memory Usage: 36 MB, less than 19.10% of Python3 online submissions for Find K Pairs with Smallest Sums.
