b0f1d5b219f47a6624945d97970e0bdf630d4c9f
Mayuko.F <66619187+Mayuko19@users.noreply.github.com>
2022-05-12 10:34:45 +0800

    Update Linked List Cycle.py

--- Linked List Cycle.py
+++ Linked List Cycle.py
@@ -21,3 +21,13 @@ class Solution(object):
 Runtime: 56 ms, faster than 66.13% of Python3 online submissions for Linked List Cycle.
 Memory Usage: 17.9 MB, less than 16.84% of Python3 online submissions for Linked List Cycle.
 """"
+
+class Solution(object):
+    def hasCycle(self, head):
+        slow, fast = head, head
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+            if slow == fast:
+                return True
+        return False
